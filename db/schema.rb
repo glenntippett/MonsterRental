53dcc2d2e9d36d82e199c914eeb849e0c0377dd9
Aya Tsurusaki <tsurusaki.yan@gmail.com>
2021-02-02 21:00:00 +1100

    create review model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_041644) do
+ActiveRecord::Schema.define(version: 2021_02_02_084422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2021_01_30_041644) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_monsters_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    # t.bigint "user_id"
+    t.string "content"
+    t.bigint "booking_id"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
