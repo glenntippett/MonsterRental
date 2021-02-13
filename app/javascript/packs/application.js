9aee086fe76b2ff7362ed3e546eac630d963b340
Josh <joshuawilliam.barnes@gmail.com>
2021-02-13 14:07:40 +1000

    added flatpickr and styling for new bookings

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,12 +23,25 @@ require("channels")
 // ----------------------------------------------------
 
 // External imports
-import "bootstrap";
+import flatpickr from "flatpickr";
+import 'flatpickr/dist/themes/material_green.css';
+import 'bootstrap';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  flatpickr('.datepicker', {
+    altInput: true,
+    // enableTime: true,
+    "disable": [
+      function(date) {
+          // return true to disable
+          return (date.getDay() === 0 || date.getDay() === 6);
+      }
+  ],
+  "locale": {
+      "firstDayOfWeek": 0 // start week on Sunday
+  }
+  });
 });
